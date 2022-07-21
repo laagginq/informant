c72a7dc1df1e2fa1d0a95d6899ab7f2d5c094e72
laagginq <95953670+laagginq@users.noreply.github.com>
2022-07-20 23:09:58 -0700

    Update notify.lua

--- files/notify.lua
+++ files/notify.lua
@@ -1,20 +1,21 @@
 local Nofitication = {}
 
-local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
-if not GUI then
-    local STX_Nofitication = Instance.new("ScreenGui")
-    local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
-    STX_Nofitication.Name = "STX_Nofitication"
-    STX_Nofitication.Parent = game.CoreGui
-    STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    STX_Nofitication.ResetOnSpawn = false
+
+local STX_Nofitication = Instance.new("ScreenGui")
+local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
+STX_Nofitication.Name = "STX_Nofitication"
+syn.protect_gui(STX_Nofitication)
+STX_Nofitication.Parent = game.CoreGui
+STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+STX_Nofitication.ResetOnSpawn = false
     
-    STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-    STX_NofiticationUIListLayout.Parent = STX_Nofitication
-    STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-end
+STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
+STX_NofiticationUIListLayout.Parent = STX_Nofitication
+STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+
+local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 function Nofitication:Notify(nofdebug, middledebug, all)
     local SelectedType = string.lower(tostring(middledebug.Type))
     local ambientShadow = Instance.new("ImageLabel")
